d9d3e5a0c3d83a4d90a53575ba32ff4f3ec472c7
silkair <silkair1234@gmail.com>
2024-10-28 20:21:56 +0900

    refactor: 컨트롤러에서 모델 분리

---
+++ src/main/java/racingcar/model/RacingGame.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RacingGame {
+    private final List<Car> cars;
+
+    public RacingGame(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void playRound() {
+        for (Car car : cars) {
+            int randomValue = Randoms.pickNumberInRange(0, 9);
+            car.move(randomValue);
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
