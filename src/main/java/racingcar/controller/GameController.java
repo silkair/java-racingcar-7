658a48736ea8587e57669888b7d52d91ce6935f9
silkair <silkair1234@gmail.com>
2024-10-28 20:21:56 +0900

    refactor: 컨트롤러에서 모델 분리

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,6 +1,10 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.Car;
+import racingcar.model.RacingGame;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameController {
     public void start() {
@@ -11,6 +15,18 @@ public class GameController {
         System.out.println("시도할 횟수는 몇 회인가요?");
         int rounds = Integer.parseInt(Console.readLine());
 
-        // 이후 로직: 자동차 생성 및 게임 진행
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+
+        RacingGame racingGame = new RacingGame(cars);
+
+        for (int i = 0; i < rounds; i++) {
+            racingGame.playRound();
+            // 각 라운드 결과를 출력하거나 다른 로직 추가
+        }
+
+        // 우승자 결정 로직 추가
     }
 }
