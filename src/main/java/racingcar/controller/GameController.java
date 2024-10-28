06731cf172bc3cb0b0820d1b242b82b005fdf91a
silkair <silkair1234@gmail.com>
2024-10-28 22:04:44 +0900

    refactor: 게임 컨트롤러 수정

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -2,30 +2,27 @@ package racingcar.controller;
 
 import racingcar.model.Car;
 import racingcar.model.RacingGame;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class GameController {
-    public void start() {
-        String[] carNames = InputView.getCarNames();
-        int rounds = InputView.getRounds();
+    private final GameSetupController setupController;
+    private final MoveController moveController;
+    private final RaceResultController resultController;
 
-        List<Car> cars = new ArrayList<>();
-        for (String name : carNames) {
-            cars.add(new Car(name));
-        }
+    public GameController(GameSetupController setupController, MoveController moveController, RaceResultController resultController) {
+        this.setupController = setupController;
+        this.moveController = moveController;
+        this.resultController = resultController;
+    }
+
+    public void start() {
+        List<Car> cars = setupController.initializeCars();
+        int rounds = setupController.initializeRounds();
 
         RacingGame racingGame = new RacingGame(cars);
 
-        for (int i = 0; i < rounds; i++) {
-            racingGame.playRound();
-            OutputView.printRoundResult(racingGame.getCars());
-        }
-
-        //우승자 출력
-        List<Car> winners = racingGame.determineWinners();
-        OutputView.printWinners(winners);
+        moveController.executeRounds(rounds);
+        resultController.printWinners();
     }
 }
