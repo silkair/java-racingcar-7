edde01c0b746495d22fb866758ae514a47396c95
silkair <silkair1234@gmail.com>
2024-10-28 20:45:07 +0900

    feat: 가장 멀리 간 자동차로 우승자 찾기

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -20,4 +20,17 @@ public class RacingGame {
     public List<Car> getCars() {
         return cars;
     }
+
+    //우승자 찾기 : 가장 멀리 간 자동차
+    public List<Car> determineWinners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .toList();
+    }
 }
+
