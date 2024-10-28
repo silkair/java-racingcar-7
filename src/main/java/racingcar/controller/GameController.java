1b18f95f7d4cb24d201da58deafca35bcd1bc332
silkair <silkair1234@gmail.com>
2024-10-28 20:39:34 +0900

    refactor: View 파일 생성 - 입력과 출력 분리

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,19 +1,16 @@
 package racingcar.controller;
 
-import camp.nextstep.edu.missionutils.Console;
 import racingcar.model.Car;
 import racingcar.model.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameController {
     public void start() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNamesInput = Console.readLine();
-        String[] carNames = carNamesInput.split(",");
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int rounds = Integer.parseInt(Console.readLine());
+        String[] carNames = InputView.getCarNames();
+        int rounds = InputView.getRounds();
 
         List<Car> cars = new ArrayList<>();
         for (String name : carNames) {
@@ -24,9 +21,10 @@ public class GameController {
 
         for (int i = 0; i < rounds; i++) {
             racingGame.playRound();
-            // 각 라운드 결과를 출력하거나 다른 로직 추가
+            OutputView.printRoundResult(racingGame.getCars());
         }
 
-        // 우승자 결정 로직 추가
+        // 우승자 결정 로직 (이후 RacingGame에 추가)
+        OutputView.printWinners(racingGame.getCars()); // 예시로 전체 자동차를 출력
     }
 }
